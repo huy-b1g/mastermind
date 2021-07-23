33769e3cce6da5e581f9e4674a58ba1da7a2af5a
dongchijoseph <huyblogging@gmail.com>
2021-07-23 13:48:39 +0700

    add some classes

---
+++ mastermind.rb
@@ -0,0 +1,55 @@
+class Maker
+  attr_reader :code
+  def initialize
+    @code = gen_code
+  end
+
+  private
+
+  def gen_code
+    code = ""
+    4.times { code += rand(1..6).to_s }
+    code.to_i
+  end
+end
+
+class Breaker
+  attr_reader :guess
+  def initialize
+    @guess = get_guess
+  end
+
+  private
+
+  def get_guess
+    puts "Please make your guess"
+    guess = gets.chomp
+    check_guess(guess)
+  end
+
+  def check_guess(guess)
+    if guess.split("").all? { |num| Array("1".."6").include?(num) } && guess.length == 4
+      return guess
+    else
+      puts "You have to choose only from 1 to 6 for only 4 digits"
+      get_guess
+    end
+  end
+end
+
+class Game
+  attr_reader :guess
+  @@code = Maker.new.code
+  @@round = 0
+
+  def initialize
+    @@round += 1
+    @guess = Breaker.new.guess
+  end
+ 
+#  private
+
+end
+
+round1 = Game.new
+puts round1.guess
